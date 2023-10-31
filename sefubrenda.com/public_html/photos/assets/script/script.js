93e04cc7eb89df5e8322a6bac84f476a9c36356b
sefufim <132181028+sefufim@users.noreply.github.com>
2023-10-30 17:59:01 -0700

    added better styling

--- sefubrenda.com/public_html/photos/assets/script/script.js
+++ sefubrenda.com/public_html/photos/assets/script/script.js
@@ -14,11 +14,13 @@ function setLanguage() {
 
     // Redirect or perform any other actions based on the selected language
     if (selectedLanguage === 'englishRadio') {
-        introText.innerHTML = "Welcome to our photos section, where you'll find our simple, heartfelt story. Our love is like a blooming flower that started with a smile and grew with every shared laugh, and memory. It's about the times we've spent together, the little moments that make life sweet, and the feeling of being truly ourselves when we're with each other. This portion of our website is extra special to us because we get to share with you the beautiful memories that we have built over the past 4 years. \n - Much love Sefu & Brenda"
+        introText.innerHTML = "Welcome to our photos section, where you'll find our simple, heartfelt story. Our love is like a blooming flower that started with a smile and grew with every shared laugh, and memory. It's about the times we've spent together, the little moments that make life sweet, and the feeling of being truly ourselves when we're with each other. This portion of our website is extra special to us because we get to share with you the beautiful memories that we have built over the past 4 years. \n - Much love Sefu & Brenda";
+        introText.classList.add('intro'); // Add the 'intro' class
     } else if (selectedLanguage === 'spanishRadio') {
-        introText.innerHTML = "Bienvenidos a nuestra sección de fotos, donde encontrarán nuestra historia simple y sincera. Nuestro amor es como una flor que florece, que comenzó con una sonrisa y creció con cada risa compartida y cada recuerdo. Se trata de los momentos que hemos pasado juntos, los pequeños instantes que hacen la vida dulce y la sensación de ser verdaderamente nosotros mismos cuando estamos el uno con el otro. Esta parte de nuestro sitio web es especialmente especial para nosotros porque podemos compartir con ustedes los hermosos recuerdos que hemos construido en los últimos 4 años.\n- Con Much amor Brenda y Sefu"
+        introText.innerHTML = "Bienvenidos a nuestra sección de fotos, donde encontrarán nuestra historia simple y sincera. Nuestro amor es como una flor que florece, que comenzó con una sonrisa y creció con cada risa compartida y cada recuerdo. Se trata de los momentos que hemos pasado juntos, los pequeños instantes que hacen la vida dulce y la sensación de ser verdaderamente nosotros mismos cuando estamos el uno con el otro. Esta parte de nuestro sitio web es especialmente especial para nosotros porque podemos compartir con ustedes los hermosos recuerdos que hemos construido en los últimos 4 años.\n- Con Much amor Brenda y Sefu";
+        introText.classList.add('intro'); // Add the 'intro' class
     }
-
+    
     // Hide the language modal
     languageModal.hide();
 }
